5ceb10fce09ae37c10385e1981f1bad3c6456fe2
piotrowo <p.kurczab94@gmail.com>
2024-05-27 18:32:22 +0200

    record naprawione

--- app/src/Controller/HelloController.php
+++ app/src/Controller/HelloController.php
@@ -5,22 +5,35 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 /**
  * Class HelloController.
  */
-class HelloController
+#[Route('/hello')]
+class HelloController extends AbstractController
 {
     /**
      * Index action.
      *
+     * @param string $name User input
+     *
      * @return Response HTTP response
      */
-    #[Route('/hello', name: 'hello_index', methods: 'GET')]
-    public function index(): Response
+    #[Route(
+        '/{name}',
+        name: 'hello_index',
+        requirements: ['name' => '[a-zA-Z]+'],
+        defaults: ['name' => 'World'],
+        methods: 'GET'
+    )]
+    public function index(string $name): Response
     {
-        return new Response('Hello world!');
+        return $this->render(
+            'hello/index.html.twig',
+            ['name' => $name]
+        );
     }
 }
